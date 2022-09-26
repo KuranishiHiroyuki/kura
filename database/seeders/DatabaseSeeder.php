a0957004777b4bc4082e5985f59fc77a4262ab0d
KuranishiHiroyuki <santa.qp.paars.mma@gmail.com>
2022-09-26 20:33:11 +0900

    usesSeeder-add

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(CategoriesSeeder::class);
+        $this->call(UsersSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            [
+                'name'    => 'test',
+                'email' => 'test@example.com',
+                'password' => Hash::make('password11'),
+                'role' => '1',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ]
+        ]);
+    }
+}
