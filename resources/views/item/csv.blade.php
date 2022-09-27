6e6505beb2049f8d4159a36d0311f27cc58d1ff3
KuranishiHiroyuki <santa.qp.paars.mma@gmail.com>
2022-09-27 13:50:27 +0900

    csv.blade.php-edit

--- resources/views/item/csv.blade.php
+++ resources/views/item/csv.blade.php
@@ -1,7 +1,7 @@
 @extends('adminlte::page')
 
 @section('content_header')
-<h1>商品登録用CSVインサート画面</h1>
+<h1>商品登録用CSVインポート画面</h1>
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/di
 st/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
